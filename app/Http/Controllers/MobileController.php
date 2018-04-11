242fcca885bf6d9651fe70f97d9e348f7ca12221
Unknown <studentac101@gmail.com>
2018-04-12 07:18:34 +0800

    v23

--- app/Http/Controllers/MobileController.php
+++ app/Http/Controllers/MobileController.php
@@ -9,5 +9,6 @@ class MobileController extends Controller
   public function search($query)
   {
     $missings = Missing::where("first_name", "like","%{$query}%")->get();
+    echo $missings;
   }
 }
