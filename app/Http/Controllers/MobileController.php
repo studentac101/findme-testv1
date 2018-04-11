b6d67e6b9a197941b881c63a887e5a6df6fb59f9
Unknown <studentac101@gmail.com>
2018-04-12 07:19:29 +0800

    v24

--- app/Http/Controllers/MobileController.php
+++ app/Http/Controllers/MobileController.php
@@ -6,8 +6,9 @@ use Illuminate\Http\Request;
 use App\Missing;
 class MobileController extends Controller
 {
-  public function search($query)
+  public function search()
   {
+    $query = "J";
     $missings = Missing::where("first_name", "like","%{$query}%")->get();
     echo $missings;
   }
